ad0e84dfd96d98922448f0f81e98d2bc344baeef
Fabrice Guillaume <fabrice.guillaume@gmail.com>
2013-01-27 14:01:23 -0500

    Initial Commit

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Fabrice Guillaume on 1/27/13.
+//  Copyright (c) 2013 Fabrice Guillaume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
